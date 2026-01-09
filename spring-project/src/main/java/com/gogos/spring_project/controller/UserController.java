3dd5a3fc8ad1d9666257860ea901d82c7d9544e6
chaudharigaurav865@gmail.com <chaudharigaurav865@gmail.com>
2026-01-10 00:34:40 +0530

    Change Postmapping

--- spring-project/src/main/java/com/gogos/spring_project/controller/UserController.java
+++ spring-project/src/main/java/com/gogos/spring_project/controller/UserController.java
@@ -5,6 +5,7 @@ import com.gogos.spring_project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class UserController {
 
     //POST-create user
 
+    @PostMapping
     public ResponseEntity<UserDto> createUser(@RequestBody UserDto userDto){
         UserDto createdUserDto=this.userService.createUser(userDto);
 
